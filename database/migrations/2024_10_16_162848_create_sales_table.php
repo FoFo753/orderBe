77c057b30d56b1a28fac2c3b259cd7da7ee41d86
trileminhfb <leminhtrisuper@gmail.com>
2024-10-16 23:35:55 +0700

    feat: create sale rank customer

---
+++ database/migrations/2024_10_16_162848_create_sales_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Enums\Sale\SaleStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('nameSale');
+            $table->string('status')->default(SaleStatus::AVAILABLE);
+            $table->dateTime('startTime');
+            $table->dateTime('endTime');
+            $table->double('percent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
