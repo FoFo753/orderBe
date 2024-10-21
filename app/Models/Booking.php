1c9ed0c91f6b45f5b4dfe78680c951738edcddf5
Chuyyy <leminhtrisuper@gmail.com>
2024-10-22 05:11:26 +0700

    feat: create controller invoice booking foods, create relationship booking cart food invoice table user

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,17 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Booking extends Model
 {
     use HasFactory;
     protected $table = "bookings";
     protected $fillable = [
-        'name',
-        'type',
-        'id_category',
-        'cost',
-        'detail',
-        'status',
+        'id_tables',
+        'timeBooking',
+        'id_food',
+        'quantity',
     ];
+
+    public function food(): HasMany
+    {
+        return $this->hasMany(Food::class);
+    }
+
+    public function table(): BelongsTo
+    {
+        return $this->belongsTo(Table::class);
+    }
 }
--- app/Models/Table.php
+++ app/Models/Table.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Table extends Model
 {
     use HasFactory;
-    protected $fillable=[
+    protected $fillable = [
         'number',
         'status',
     ];
